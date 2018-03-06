c52b7aa5a91f66b6058c640cbd8db4a4c212137e
Mike Brooks <mikebrooks92@gmail.com>
2018-03-05 22:12:18 -0500

    Initiate project and built drawable canvas

---
+++ freakies/Assets/Scripts/Line.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+	public LineRenderer lineRenderer;
+
+	private List<Vector2> points;
+
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
+
+	public void UpdateLine (Vector2 point) {
+		if (points == null) {
+			points = new List<Vector2> ();
+			SetPoint (point);
+		}
+
+		if (Vector2.Distance (points.Last (), point) > .1f) {
+			SetPoint (point);
+		}
+	}
+
+	private void SetPoint(Vector2 point) {
+		points.Add (point);
+		lineRenderer.positionCount = points.Count;
+		lineRenderer.SetPosition (points.Count - 1, point);
+	}
+}
