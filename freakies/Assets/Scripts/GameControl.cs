0f9a28fa38fe266f537c16d93305a42a42993efe
Jacob Friedmann <jacobdfriedmann@gmail.com>
2018-03-20 21:18:02 -0700

    Refactoring

--- freakies/Assets/Scripts/GameControl.cs
+++ freakies/Assets/Scripts/GameControl.cs
@@ -3,70 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+enum Scene { Head, Body }
+
 public class GameControl : MonoBehaviour {
 	public List<Line> headLines;
 	public List<Line> bodyLines;
 	public Button undoButton;
-	private bool clicklingButton;
-
+	public Button doneButton;
 	public GameObject linePrefab;
 
+	private bool undoButtonDepressed = false;
 	private Line activeLine;
-	private List<List<Vector3>> lineVectors;
-	private Stack<Line> lines;
-	private BoxCollider2D undoCollider;
+	private List<List<Vector3>> headLineVectors = new List<List<Vector3>> ();
+	private List<List<Vector3>> bodyLineVectors = new List<List<Vector3>> ();
+	private List<List<Vector3>> activeVectorList;
+	private Stack<Line> lineHistory;
 
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad (gameObject);
-		clicklingButton = false;
-		undoButton.onClick.AddListener (ButtonClicked);
-		lineVectors = new List<List<Vector3>> ();
-		lines = new Stack<Line> ();
-		print ("X: " + undoButton.transform.position.x + " Y: " + undoButton.transform.position.y + " Z: " + undoButton.transform.position.z);
-		undoCollider = undoButton.GetComponent<BoxCollider2D> ();
-		print(undoCollider.size);
-
+		undoButton.onClick.AddListener (Undo);
+		doneButton.onClick.AddListener (Next);
+		StartScene (Scene.Head);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		HandleUndoButton ();
+		if (!undoButtonDepressed) {
+			HandleLine ();
+		}
+
+	}
+
+	void StartScene (Scene scene) {
+		lineHistory = new Stack<Line> ();
+		activeVectorList = scene.Equals (Scene.Head) ? headLineVectors : bodyLineVectors;
+	}
+
+	void Undo () {
+		undoButtonDepressed = true;
+		activeVectorList.RemoveAt (activeVectorList.Count - 1);
+		Destroy (lineHistory.Pop ().gameObject);
+	}
+
+	void Next () {
+		Application.LoadLevel(1);
+		StartScene (Scene.Body);
+	}
+
+	void HandleUndoButton () {
 		if (Input.GetMouseButtonUp (0)) {
-			clicklingButton = false;
-			print ("Mouse button up " + clicklingButton);
+			undoButtonDepressed = false;
 		}
-		if (!clicklingButton) {
-			DrawLine ();
-		}
-
 	}
 
-	void ButtonClicked () {
-		clicklingButton = true;
-		Destroy (lines.Pop ().gameObject);
-		print ("I HAVE BEEN CLICKED! " + clicklingButton);
-	}
-
-	void DrawLine () {
+	void HandleLine () {
 		Vector2 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-		if (Input.GetMouseButtonDown (0)) {
-			if (hit != null && hit.collider != null) {
-				GameObject newLine = Instantiate (linePrefab);
-				activeLine = newLine.GetComponent<Line> ();
-			}
+		bool isInCanvas = hit != null && hit.collider != null;
+		bool isLineActive = activeLine != null;
 
-		} else if (Input.GetMouseButtonUp (0) || (hit == null || hit.collider == null)) {
-			if (activeLine != null) {
-				lineVectors.Add (activeLine.GetPoints());
-				lines.Push (activeLine);
-			}
+		if (isLineActive && (Input.GetMouseButtonUp (0) || !isInCanvas)) {
+			activeVectorList.Add (activeLine.GetPoints ());
+			lineHistory.Push (activeLine);
 			activeLine = null;
-		}
-
-		if (activeLine != null) {
+		} else if (isLineActive) {
 			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			activeLine.UpdateLine (mousePos);
+		} else if (Input.GetMouseButtonDown (0) && isInCanvas) {
+			GameObject newLine = Instantiate (linePrefab);
+			activeLine = newLine.GetComponent<Line> ();
 		}
 	}
 }
