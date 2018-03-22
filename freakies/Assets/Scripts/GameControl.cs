e6a0ce1a38e79bf60754141143d13ed495278c5e
Jacob Friedmann <jacobdfriedmann@gmail.com>
2018-03-22 06:46:17 -0700

    Redraw head and body

--- freakies/Assets/Scripts/GameControl.cs
+++ freakies/Assets/Scripts/GameControl.cs
@@ -12,12 +12,13 @@ public class GameControl : MonoBehaviour {
 	public GameObject linePrefab;
 
 	private FreakyScene currentScene;
-	private bool undoButtonDepressed = false;
 	private Line activeLine;
-	private List<List<Vector3>> headLineVectors;
-	private List<List<Vector3>> bodyLineVectors;
+	private List<List<Vector3>> headLineVectors = new List<List<Vector3>> ();
+	private List<List<Vector3>> bodyLineVectors = new List<List<Vector3>> ();
 	private List<List<Vector3>> activeVectorList;
 	private Stack<Line> lineHistory;
+
+	// Lifecycle
 
 	void Awake () {
 		if (Control == null) {
@@ -28,45 +29,19 @@ public class GameControl : MonoBehaviour {
 		}
 	}
 
-	// Use this for initialization
 	void Start () {
-		headLineVectors = headLineVectors == null ? new List<List<Vector3>> () : headLineVectors;
-		bodyLineVectors = bodyLineVectors == null ? new List<List<Vector3>> () : bodyLineVectors;
 		currentScene = (FreakyScene)SceneManager.GetActiveScene ().buildIndex;
 		StartScene (SceneManager.GetActiveScene (), LoadSceneMode.Single);
 		SceneManager.sceneLoaded += StartScene;
 	}
 
-	// Update is called once per frame
 	void Update () {
-		HandleUndoButton ();
-		if (!undoButtonDepressed) {
-			HandleLine ();
-		}
-
+		DrawLine ();
 	}
 
-	void StartScene (Scene scene, LoadSceneMode mode) {
-		Debug.Log (scene.buildIndex);
-		FreakyScene freakyScene = (FreakyScene)scene.buildIndex;
-		if (freakyScene == FreakyScene.Head || freakyScene == FreakyScene.Body) {
-			lineHistory = new Stack<Line> ();
-			activeVectorList = scene.Equals (FreakyScene.Head) ? headLineVectors : bodyLineVectors;
-		} else if (freakyScene == FreakyScene.Reveal) {
-			Debug.Log ("Making lines!");
-			Debug.Log ("Head lines are: " + headLineVectors.Count);
-			headLineVectors.ForEach (delegate(List<Vector3> vectors) {
-				GameObject newLineObj = Instantiate (linePrefab);
-				Line newLine = newLineObj.GetComponent<Line> ();
-				vectors.ForEach (delegate(Vector3 vector) {
-					newLine.UpdateLine ((Vector2)vector);
-				});
-			});
-		}
-	}
+	// Public methods
 
 	public void Undo () {
-		undoButtonDepressed = true;
 		activeVectorList.RemoveAt (activeVectorList.Count - 1);
 		Destroy (lineHistory.Pop ().gameObject);
 	}
@@ -76,13 +51,21 @@ public class GameControl : MonoBehaviour {
 		SceneManager.LoadScene((int)currentScene);
 	}
 
-	void HandleUndoButton () {
-		if (Input.GetMouseButtonUp (0)) {
-			undoButtonDepressed = false;
+	// Event handlers
+
+	void StartScene (Scene scene, LoadSceneMode mode) {
+		FreakyScene freakyScene = (FreakyScene)scene.buildIndex;
+		if (freakyScene == FreakyScene.Head || freakyScene == FreakyScene.Body) {
+			lineHistory = new Stack<Line> ();
+			activeVectorList = freakyScene.Equals (FreakyScene.Head) ? headLineVectors : bodyLineVectors;
+		} else if (freakyScene == FreakyScene.Reveal) {
+			DrawFreakie ();
 		}
 	}
 
-	void HandleLine () {
+	// Private methods
+
+	void DrawLine () {
 		Vector2 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
 		bool isInCanvas = hit != null && hit.collider != null;
@@ -99,5 +82,23 @@ public class GameControl : MonoBehaviour {
 			GameObject newLine = Instantiate (linePrefab);
 			activeLine = newLine.GetComponent<Line> ();
 		}
+	}
+
+	void DrawFreakie () {
+		headLineVectors.ForEach (delegate(List<Vector3> vectors) {
+			GameObject newLineObj = Instantiate (linePrefab);
+			Line newLine = newLineObj.GetComponent<Line> ();
+			vectors.ForEach (delegate(Vector3 vector) {
+				newLine.UpdateLine ((Vector2)vector);
+			});
+			newLine.lineRenderer.transform.localScale = new Vector3(0.1f, 0.1f, 0);
+		});
+		bodyLineVectors.ForEach (delegate(List<Vector3> vectors) {
+			GameObject newLineObj = Instantiate (linePrefab);
+			Line newLine = newLineObj.GetComponent<Line> ();
+			vectors.ForEach (delegate(Vector3 vector) {
+				newLine.UpdateLine ((Vector2)vector);
+			});
+		});
 	}
 }
